e09c3b7d60ccb4e94df982828308becf509cd166
ginevrar <ginevra.rosati@gmail.com>
2018-06-21 23:23:33 +0200

    v

--- MELa_dati_by_area_mese_plots.R
+++ MELa_dati_by_area_mese_plots.R
@@ -1,4 +1,4 @@
-setwd('C:/Users/Ginevra/Dropbox/fin37')
+setwd('C:/Users/gi/Dropbox/fin39')
 
 hg<-read.csv('Total_Hg.csv', skip=1)
 names(hg)<-c('time','wn1','wn2','wn3','wn4','wn5','wc6','wc7','ws8','ws9','ws10', 
